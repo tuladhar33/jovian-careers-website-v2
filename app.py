0b7c7219f8ba3d6907398d0b7da88eafaa4d5498
tuladhar <80570308+tuladhar33@users.noreply.github.com>
2023-04-09 06:24:20 +0000

    added dynamic data and template

--- app.py
+++ app.py
@@ -2,9 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 15,00,000'
+}, {
+  'id': 3,
+  'title': 'FrontEnd Engineer',
+  'location': 'Remote',
+  'salary': 'Rs. 12,00,000'
+}, {
+  'id': 3,
+  'title': 'BackEnd Engineer',
+  'location': 'San Francisco, USA',
+  'salary': '$ 12,000'}
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs = JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
